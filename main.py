b98cba6cbbebfd673e27fe5c56b2e111ac1f85b9
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-10-05 19:07:06 -0300

    feat: add flight controller to add and delete flight

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # Add the project root directory to sys.path
 import os
 import sys
+
+from numpy import delete
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 # Task 2, 5, 9: Import modules here
 from models.auth import CLIAuthenticator 
 from controllers.user_controller import UserController  
+from controllers.flight_controller import FlightController
 
 
 # Task 2, 6, 10, 18: Redefine the main() function
@@ -34,7 +37,8 @@ def main():
 
     # If login is successful, proceed with reservations
     if user_controller.current_user:
-        pass
+    
+        flight_controller = FlightController()
 
         while True:
             print("1. Search/Reserve Flights")
@@ -52,9 +56,9 @@ def main():
             elif choice == "3":
                 print("Here to cancel flight reservations.")
             elif choice == "4":
-                print("Here to add new flight.")
+                flight_controller.add_flight(user_controller.current_user)
             elif choice == "5":
-                print("Here to cancel a flight.")
+                flight_controller.delete_flight(user_controller.current_user)
             elif choice == "6":
                 break
             else:
