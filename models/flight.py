b74ade5121adb5bf1590725fe1ab50ec75ef1a66
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-10-07 08:08:18 -0300

    refactor: add types

--- models/flight.py
+++ models/flight.py
@@ -1,10 +1,24 @@
 
-# Task 14: Import the Amadeus components 
+# Task 14: Import the Amadeus components
 
 
 # Task 3: Define the Flight class
+import datetime
+from turtle import distance
+
+
 class Flight:
-    def __init__(self, airline_code, distance_km, dep_time, arri_time, dep_port, arri_port, booked_seats = 0, flight_no = None):
+    __flight_no: int
+    __airline_code: str
+    __distance_km: float
+    __dep_time: datetime.datetime
+    __arri_time: datetime.datetime
+    __dep_port: str
+    __arri_port: str
+    __booked_seats: int
+    __total_seats: int
+
+    def __init__(self, flight_no: int, airline_code: str, distance_km: float, dep_time: datetime.datetime, arri_time: datetime.datetime, dep_port: str, arri_port: str, booked_seats: int = 0):
         self.__flight_no = flight_no
         self.__airline_code = airline_code
         self.__distance_km = distance_km
@@ -13,7 +27,7 @@ class Flight:
         self.__dep_port = dep_port
         self.__arri_port = arri_port
         self.__total_seats = 50  # Set total seats to 50
-        self.__booked_seats = 0  # Initialize booked seats to 0
+        self.__booked_seats = booked_seats  # Initialize booked seats to 0
 
     # Getters
     def get_flight_no(self):
@@ -40,46 +54,42 @@ class Flight:
     # Method to get the number of available seats
     def get_available_seats(self):
         return self.__total_seats - self.__booked_seats
-
-
     # Setters
-    def set_flight_no(self, flight_no):
+    def set_flight_no(self, flight_no: int):
         self.__flight_no = flight_no
 
-    def set_airline_code(self, airline_code):
+    def set_airline_code(self, airline_code: str):
         self.__airline_code = airline_code
 
-    def set_dep_time(self, dep_time):
+    def set_dep_time(self, dep_time: datetime.datetime):
         self.__dep_time = dep_time
 
-    def set_arri_time(self, arri_time):
+    def set_arri_time(self, arri_time: datetime.datetime):
         self.__arri_time = arri_time
 
-    def set_dep_port(self, dep_port):
+    def set_dep_port(self, dep_port: str):
         self.__dep_port = dep_port
 
-    def set_arri_port(self, arri_port):
+    def set_arri_port(self, arri_port: str):
         self.__arri_port = arri_port
 
-    def set_dep_port(self, dep_port):
-        self.__dep_port = dep_port
-
     # Book seats
-    def book_seats(self, num_seats):
+    def book_seats(self, num_seats: int):
         if num_seats <= self.get_available_seats():
             self.__booked_seats += num_seats
             print(f"{num_seats} seat(s) successfully booked.")
         else:
-            print(f"Not enough seats available. Only {self.get_available_seats()} left.")
+            print(f"Not enough seats available. Only {
+                  self.get_available_seats()} left.")
 
     # Seat availability
     def get_total_seats(self):
-        return len(self.__seats)
+        return self.__total_seats
 
     # String representation
     def __repr__(self):
         return (f"Flight No: {self.__flight_no}, Airline Code: {self.__airline_code}, "
-        f"Departure from: {self.__dep_port} at {self.__dep_time}, "
-        f"Arrival at: {self.__arri_port} at {self.__arri_time}")
+                f"Departure from: {self.__dep_port} at {self.__dep_time}, "
+                f"Arrival at: {self.__arri_port} at {self.__arri_time}")
 
-    # Task 14: Implement the get_ticket_price() function 
+    # Task 14: Implement the get_ticket_price() function
